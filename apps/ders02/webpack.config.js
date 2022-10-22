9ab72d986fad5770cd932f11043f79dbfbb53cd9
Omur <omur@visseria.com>
2022-10-22 19:41:36 +0300

    Dersler tamamlandı.

--- apps/ders02/webpack.config.js
+++ apps/ders02/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname,'public'),
